109835c6f332f656ed03c7aacdc42ba2e1e7fb64
mangraa1 <georgiu.spare@gmail.com>
2024-02-21 17:23:50 +0200

    Hi

---
+++ Testproj/Testproj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testproj
+//
+//  Created by mac on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        print("Hi 1")
+        print("Hi 1")
+        print("Hi 1")
+        print("Hi 1")
+        print("Hi 1")
+    }
+
+
+}
+
